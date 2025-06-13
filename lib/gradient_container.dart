445034e317d06b76462ebdbe174c908827a8b209
AhmadNasserx <111495732+AhmadNasserx@users.noreply.github.com>
2025-06-13 11:51:58 +0300

    colors optimization

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -4,24 +4,22 @@ import 'package:first_app/styled_text.dart';
 const startAlignment = Alignment.topLeft;
 const endAlignment =  Alignment.bottomRight;
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colors});
+
+    final List<Color> colors;
 
   @override
   Widget build(context) {
     return  Container(
-          decoration: const BoxDecoration(
+          decoration:  BoxDecoration(
             gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 26, 2, 80),
-                Color.fromARGB(255, 45, 7, 98),
-                Color.fromARGB(255, 66, 12, 148),
-              ],
+              colors: colors,
               begin: startAlignment,
               end: endAlignment,
             ),
           ),
           child: const Center(
-            child: StyledText()
+            child: StyledText('Hello World!')
           ),
         );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:first_app/gradient_container.dart';
+
+
+
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: GradientContainer()
+        body: GradientContainer(
+          colors: [
+            Color.fromARGB(255, 26, 2, 80),
+            Color.fromARGB(255, 45, 7, 98),
+            Color.fromARGB(255, 66, 12, 148),
+          ],
+)
       ),
     ),
   );
 }
-
