57e149af2d5ca2342a4257f1dae3d928b344fab2
AhmadNasserx <111495732+AhmadNasserx@users.noreply.github.com>
2025-06-13 18:41:52 +0300

    Logic and final Commit

--- lib/dice_roller.dart
+++ lib/dice_roller.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+
+final randomizer = Random();
 
 class DiceRoller extends StatefulWidget{
   const DiceRoller({super.key});
@@ -11,10 +15,10 @@ class DiceRoller extends StatefulWidget{
 }
 
 class _DiceRollerState extends State<DiceRoller>{
-      var activeDiceImage = 'assets/images/dice-2.png';
+  var currentDiceRoll = 2;
     rollDice() {
       setState(() {
-         activeDiceImage = 'assets/images/dice-4.png';
+         currentDiceRoll = randomizer.nextInt(6) + 1;
       });
     }
   @override
@@ -23,7 +27,8 @@ class _DiceRollerState extends State<DiceRoller>{
               mainAxisSize:MainAxisSize.min ,
               children: [
                   Image.asset(
-                    activeDiceImage, width: 200,
+                    'assets/images/dice-$currentDiceRoll.png',
+                     width: 200,
                   ),
                   TextButton(onPressed: rollDice
                   ,
